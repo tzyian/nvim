8e47da6d780d13d501eba28f3fc2ba0be742fd1c
ian <git@zyt.slmail.me>
2026-04-02 23:42:32 +0800

    add wc to lualine

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -14,13 +14,40 @@ return {
 			end
 		end
 
+		local function wordcount()
+			local label = "word"
+			local word_count = 0
+
+			if vim.fn.mode():find("[vV]") then
+				word_count = vim.fn.wordcount().visual_words
+			else
+				word_count = vim.fn.wordcount().words
+			end
+
+			if word_count ~= 1 then
+				label = label .. "s"
+			end
+
+			return word_count .. " " .. label
+		end
+
+
+		local filetypes = { "typst", "markdown", "text", "tex" }
+
 		require("lualine").setup({
 			sections = {
 				lualine_b = { { "b:gitsigns_head", icon = "" }, { "diff", source = diff_source }, "diagnostics" },
+				lualine_x = { {
+					wordcount,
+					cond = function()
+						return vim.tbl_contains(filetypes, vim.bo.filetype)
+					end
+				}, "encoding", "fileformat", "filetype" },
 			},
 			options = {
 				theme = "catppuccin-nvim",
 			},
+			extensions = { "nvim-tree", }
 		})
 	end,
 }
